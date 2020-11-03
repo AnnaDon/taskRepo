aa843503f1d782ad5f936fe1ba3a917cc5c68a38
AnnaDon <doncilaas@gmail.com>
2020-11-03 17:16:43 +0200

    constructor and toString were added

--- module/src/Main.java
+++ module/src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public Main() {
+    }
+
+    @Override
+    public String toString() {
+        return "Main{}";
+    }
 }
